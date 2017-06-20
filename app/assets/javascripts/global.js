cce2088a28705ca67becc619380d288ef290ece1
Brian Whitmer <brian.whitmer@gmail.com>
2017-06-20 15:31:50 -0600

    tweaks

---
+++ app/assets/javascripts/global.js
@@ -0,0 +1,16 @@
+var doc_lookup = function(doc_id, cell, success, error) {
+  cell = cell.replace(/A17$/, '');
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      res = JSON.parse(this.responseText);
+      success(res);
+    } else if(this.readyState == 4) {
+      error();
+    }
+  };
+  var path = "/data/" + doc_id + "/" + cell;
+  xhr.open('GET', path, true);
+  xhr.send();
+}
+window.doc_lookup = doc_lookup;
