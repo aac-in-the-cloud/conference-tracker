9ddf7e0b9b06ed25c56dffdce48a4cbae49bb292
Brian Whitmer <brian.whitmer@gmail.com>
2018-06-19 14:24:41 -0600

    cache conference data

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -1,56 +1,60 @@
 class ConferencesController < ApplicationController
   def show
     response.headers.except! 'X-Frame-Options'
-    sessions = ConferenceSession.where(:conference_code => params['id'])
     conference = Conference.find_by(code: params['id'])
     if !conference
       render text: "Invalid Conference"
       return
     end
-    conference_json = JSON.parse(conference.data) rescue nil
-    conference_json ||= {}
-    @conference = {
-      name: conference.name,
-      code: conference.code,
-      year: "20#{params['id'].match(/\d+/)[0]}",
-      closed: !!conference_json['closed'],
-      tracks: [],
-      days: []
-    }
-    sessions = sessions.select{|s| s.resources }
-    days = sessions.group_by{|s| s.resources['timestamp'][0, 10] }.to_a.sort_by(&:first)
-    max_tracks = sessions.group_by{|s| s.resources['timestamp']}.to_a.map(&:last).map(&:length).max || 0
-
-    days.each do |day, list|
-      day = {
-        name: Date.parse(day).strftime('%B %e, %Y'),
-        time_slots: []
+    @conference = Rails.cache.fetch("conference/#{params['id']}", expires_in: 15.minutes) do
+      conference_json = JSON.parse(conference.data) rescue nil
+      conference_json ||= {}
+      sessions = ConferenceSession.where(:conference_code => params['id'])
+      conference = {
+        name: conference.name,
+        code: conference.code,
+        year: "20#{params['id'].match(/\d+/)[0]}",
+        closed: !!conference_json['closed'],
+        tracks: [],
+        days: []
       }
-      time_slots = list.group_by{|s| s.resources['timestamp']}.to_a.sort_by(&:first)
-      time_slots.each do |timestamp, list|
-        time = Time.parse(timestamp)
-        name = time.min == 0 ? time.strftime('%l %P ET') : time.strftime('%l:%M %P ET')
-        slot = {
-          name: name,
-          tracks: [],
-          special: (max_tracks > 1 && list.length == 1)
+      sessions = sessions.select{|s| s.resources }
+      days = sessions.group_by{|s| s.resources['timestamp'][0, 10] }.to_a.sort_by(&:first)
+      max_tracks = sessions.group_by{|s| s.resources['timestamp']}.to_a.map(&:last).map(&:length).max || 0
+
+      days.each do |day, list|
+        day = {
+          name: Date.parse(day).strftime('%B %e, %Y'),
+          time_slots: []
         }
-        max_tracks = [max_tracks, list.length].max
-        list.sort_by(&:code).each do |session|
-          track = {
-            name: session.resources['session_name'],
-            link: session.video_link,
-            slack: session.slack_text
+        time_slots = list.group_by{|s| s.resources['timestamp']}.to_a.sort_by(&:first)
+        time_slots.each do |timestamp, list|
+          time = Time.parse(timestamp)
+          name = time.min == 0 ? time.strftime('%l %P ET') : time.strftime('%l:%M %P ET')
+          slot = {
+            name: name,
+            tracks: [],
+            special: (max_tracks > 1 && list.length == 1)
           }
-          slot[:tracks] << track
+          max_tracks = [max_tracks, list.length].max
+          list.sort_by(&:code).each do |session|
+            track = {
+              name: session.resources['session_name'],
+              link: session.video_link,
+              slack: session.slack_text
+            }
+            slot[:tracks] << track
+          end
+          day[:time_slots] << slot
         end
-        day[:time_slots] << slot
+        conference[:days] << day
       end
-      @conference[:days] << day
+      max_tracks.times do |i|
+        conference[:tracks] << "Track #{i + 1}"
+      end
+      conference
     end
-    max_tracks.times do |i|
-      @conference[:tracks] << "Track #{i + 1}"
-    end
+    @conference = @conference.with_indifferent_access
   end
 
   def manage_session
